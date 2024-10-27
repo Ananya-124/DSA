6ce669099461c51bbe86af347df75fb087f1ee49
Ananya <ananyaananyaa124@gmail.com>
2024-10-27 16:27:37 +0530

    new

--- Problems.py
+++ Problems.py
@@ -1,7 +1,17 @@
-dict = {'name':'ananya','number':[1,2,3,4,5]}
-print("\nNormal dictionary",dict)
-print("\nkey name\n",dict['name'])
-print("key number\n",dict['number'])
-print('\nusing get\n',dict.get(0))
-dicti = {x:x**2 for x in [1,2,3,4,5]}
-print(dicti)
+# Creating bytearray
+a = bytearray((12, 8, 25, 2))
+print("Creating Bytearray:")
+print(a)
+
+# accessing elements
+print("\nAccessing Elements:", a[1])
+
+# modifying elements
+a[1] = 3
+print("\nAfter Modifying:")
+print(a)
+
+# Appending elements
+a.append(30)
+print("\nAfter Adding Elements:")
+print(a)
