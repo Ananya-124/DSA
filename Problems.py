8f7526025e050df0853c91237bc006f0f19df2d2
Ananya <ananyaananyaa124@gmail.com>
2024-10-27 16:07:54 +0530

    new

---
+++ Problems.py
@@ -0,0 +1,7 @@
+dict = {'name':'ananya','number':[1,2,3,4,5]}
+print("\nNormal dictionary",dict)
+print("\nkey name\n",dict['name'])
+print("key number\n",dict['number'])
+print('\nusing get\n',dict.get(0))
+dicti = {x:x**2 for x in [1,2,3,4,5]}
+print(dicti)
